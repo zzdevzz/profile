e7f9aa530612ef42686664dcf867811e4239e7da
dev <dev.h@hotmail.co.uk>
2024-04-17 14:05:01 +0100

    javascript final

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const memes = document.getElementById("containerMeme")
+const buttonEl = document.getElementById("buttonEl")
 
-document.getElementById("buttonEl").addEventListener("click", function(){
+buttonEl.addEventListener("click", function(){
   memes.classList.toggle("show");
   window.scrollBy({
     top: memes.classList.contains("show") ? 500 : -350,
     left: 0,
     behavior: 'smooth'
   })
+  buttonEl.textContent = memes.classList.contains("show") ? "😔 Ahh Unlucky... Click to Collapse" : "🍕 Wait... There's More Free Pizza!"
 })
